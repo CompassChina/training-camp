f7b3fee5ba96d78c4abcfc56e2e6d58325c243e2
Liqiang Yang <liqiang.yang@compass.com>
2022-07-05 02:08:41 +0800

    Add #2_FetchAllCompanies

--- questions-view/src/config/menu.tsx
+++ questions-view/src/config/menu.tsx
@@ -6,6 +6,8 @@ import { Template } from '../Template'
 import NumberInput_Readme from '../../../1_NumberInput/README.md?raw'
 import { Demo as NumberInput } from '../../../1_NumberInput/src/Demo'
 
+import FetchAllCompanies_Readme from '../../../2_FetchAllCompanies/README.md?raw'
+
 export default [
   {
     label: '首页',
@@ -23,6 +25,18 @@ export default [
         demo={<NumberInput />}
         repo='https://github.com/CompassChina/compass-fe-camp--q1-NumberInput'
         slack='https://compass-china.slack.com/archives/C03HJ00CXDK'
+      />
+    ),
+  },
+  {
+    label: '#2：FetchAllCompanies',
+    icon: <CheckCircleTwoTone />,
+    path: 'question/2',
+    element: (
+      <Template
+        content={FetchAllCompanies_Readme}
+        repo='https://github.com/CompassChina/compass-fe-camp--q2-fetchAllCompanies'
+        slack='https://compass-china.slack.com/archives/C03MJ8LEKPT'
       />
     ),
   },
