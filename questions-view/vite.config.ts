523e7c5009f06502412b78165cbefb1a910ad126
Liqiang Yang <liqiang.yang@compass.com>
2022-07-05 00:24:31 +0800

    Config Vite output

--- questions-view/vite.config.ts
+++ questions-view/vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   root: './src',
   plugins: [react()],
+  build: {
+    emptyOutDir: true,
+    chunkSizeWarningLimit: 1000,
+    outDir: '../../docs',
+    assetsDir: '',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'index.js',
+        assetFileNames: 'index.[ext]',
+      },
+    },
+  },
 })
